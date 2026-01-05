1c9932fde49f5e1b968c39ba2081385a28285a77
Abyss <95066028+ezabyss@users.noreply.github.com>
2026-01-05 10:45:12 +0545

    Create commands.sh

---
+++ Course-04-Tools-of-the-Trade-Linux-and-SQL/Module-02-The-Linux-Operating-System/labs/lab-02-input-output-bash/commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Generate output using echo
+echo hello
+echo "hello"
+echo "Ez Abyss"
+
+# Perform calculations using expr
+expr 32 - 8
+expr 3500 \* 12
+
+# Optional exploration
+echo "Exploring input and output in Bash"
+expr 25 + 15
+
+# Clear the terminal
+clear
