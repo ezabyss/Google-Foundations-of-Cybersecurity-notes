22b44554ab0358fa897aeb5a40f2d4e149295a88
Abyss <95066028+ezabyss@users.noreply.github.com>
2026-01-09 12:03:36 +0545

    Create commands.sh

---
+++ Course-04-Tools-of-the-Trade-Linux-and-SQL/Module-03-Linux-Commands-in-the-Bash-Shell/labs/lab-06-get-help-in-the-commandline/commands.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Task 1: Get a short description of the cat command
+whatis cat
+
+# View the manual page for the cat command
+man cat
+
+# Task 1 (continued): Search for a command that returns the first part of a file
+# -a ensures all keywords must match
+apropos -a first part file
+
+# Task 2: Explore options available for the useradd command
+man useradd
+
+# Task 3: Get short descriptions of rm and rmdir to understand the difference
+whatis rm
+whatis rmdir
+
+# Task 4: Identify the command used to create a new group
+apropos -a create new group
